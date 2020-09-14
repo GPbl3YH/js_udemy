a1772418f077c7b9676d4d0a1c88fa2c308cee67
Oleg <prytkov.2213@gmail.com>
2020-09-14 20:30:14 +0300

    Practice 2

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+
+while(numberOfFilms <= 0)
+{
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+}
+
+
 const personalMovieDB = {
     count : numberOfFilms,
     movies : {},
@@ -10,11 +17,29 @@ const personalMovieDB = {
     privat : false
 };
 
-let lastMovie, rating;
 
-for (let x = 0; x < 2; x++){
+let lastMovie, rating, x = 0;
+
+
+while (x < numberOfFilms){
     lastMovie = prompt('Один из последних фильмов?', '');
-    rating = +prompt('Оцнка?', '');
-    personalMovieDB.movies[lastMovie] = rating;
+    while (lastMovie.length > 50 || lastMovie.length == 0 )
+    {
+        lastMovie = prompt('Один из последних фильмов?', '');
+    }
+    rating = prompt('Оценка?', '');
+    while (rating.length == 0 || +rating > 10 || +rating < 0)
+    {
+        rating = prompt('Оценка?', '');
+    }
+    personalMovieDB.movies[lastMovie] = +rating;
+    x++;
 }
+
+if(personalMovieDB.count < 10) console.log("Просмотрено мало фильмов");
+else if(10 <= personalMovieDB.count <= 50) console.log("Вы - классический зритель");
+else console.log("Вы - заядлый киноман");
+
+
+
 console.log(personalMovieDB);
